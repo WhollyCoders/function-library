3440b99cb66f820daa47958e05edaded329f6b69
Michael L Parks <github@wmi-inc.com>
2018-04-28 08:12:20 -0400

    Initial Commit

---
+++ js/functlib.js
@@ -0,0 +1,21 @@
+function getDayOfWeek(day){
+  if(day == 0){
+    return "Sunday";
+  }
+  if(day == 1){
+    return "Monday";
+  }
+  if(day == 2){
+    return "Tuesday";
+  }
+  if(day == 3){
+    return "Wednesday";
+  }
+  if(day == 4){
+    return "Thursday";
+  }
+  if(day == 5){
+    return "Friday";
+  }
+    return "Saturday";
+}
